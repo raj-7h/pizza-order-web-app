a016717c65bf6453dc6da804a515f13947fda4b7
raj-7h <rajjha.study@gmail.com>
2025-07-10 01:43:23 +0530

    Deploy-backend

--- src/api/getPastOrder.js
+++ src/api/getPastOrder.js
@@ -2,7 +2,9 @@ export default async function getPastOrder(order) {
   try {
     if (!order) return null;
     await new Promise((resolve) => setTimeout(resolve, 5000));
-    const response = await fetch(`/api/past-order/${order}`);
+    const response = await fetch(
+      `https://pizza-backend-xpbk.onrender.com/api/past-order/${order}`,
+    );
     if (!response.ok) {
       console.warn(`⚠️ API response with status ${response.status}`);
       return null;

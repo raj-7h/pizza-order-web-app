c22a453ee44582fea2a15ad3e147e6238beb8cbf
raj-7h <rajjha.study@gmail.com>
2025-07-09 22:58:52 +0530

    Updated 3 API routes to point to Render backend

--- src/api/postContact.js
+++ src/api/postContact.js
@@ -1,11 +1,14 @@
 export default async function postContact(name, email, message) {
-  const response = await fetch("/api/contact", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
+  const response = await fetch(
+    "https://pizza-backend-xpbk.onrender.com/api/contact",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, email, message }),
     },
-    body: JSON.stringify({ name, email, message }),
-  });
+  );
   if (!response.ok) {
     throw new Error("Network response was not ok. Send help.");
   }
